9dbc7520850fcf89c14eddb32e7059722ef7c44f
Craudo-boy <dougmorona0@gmail.com>
2021-04-24 20:45:48 -0300

    Criado menus e nomes para a máquina.

--- src/main/java/com/iel/view/Principal.java
+++ src/main/java/com/iel/view/Principal.java
@@ -5,6 +5,7 @@
  */
 package com.iel.view;
 
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
@@ -17,9 +18,44 @@ public class Principal extends javax.swing.JFrame {
     public boolean qmJoga = true;
     public String[] arrayJogo = new String[9];
     public int tamTab = 3;
+    public String[] nomes = new String[12];
 
     public Principal() {
+        nomes[0] = "Magalu";
+        nomes[1] = "BIA";
+        nomes[2] = "Mudae";
+        nomes[3] = "Bahianinho.old";
+        nomes[4] = "SkyNet";
+        nomes[5] = "CB";
+        nomes[6] = "Pedro";
+        nomes[7] = "Garoto de Programa";
+        nomes[8] = "Craudo";
+        nomes[9] = "Megumin";
+        nomes[10] = "NatNatu";
+        
         initComponents();
+
+    }
+
+    public void geraJogoPvp() {
+        lblJogador1.setText(pegaNome("Jogador1"));
+        lblJogador2.setText(pegaNome("Jogador2"));
+    }
+
+    public void geraJogoPvm() {
+        Random nomeM = new Random();
+        nomes[11] = pegaNome("Jogador1");
+        lblJogador1.setText(nomes[11]);
+        lblJogador2.setText(nomes[nomeM.nextInt(11)]);
+    }
+
+    public String pegaNome(String jogador) {
+        String nomeJ = JOptionPane.showInputDialog(this, "Insira o nome do " + jogador, "Nome do jogador", JOptionPane.INFORMATION_MESSAGE);
+        if (nomeJ == null || nomeJ.isEmpty()) {
+
+            nomeJ = pegaNome(jogador);
+        }
+        return nomeJ;
     }
 
     /**
@@ -46,6 +82,14 @@ public class Principal extends javax.swing.JFrame {
         btnDoisUm = new javax.swing.JButton();
         btnDoisDois = new javax.swing.JButton();
         lblAtaul = new javax.swing.JLabel();
+        mnuBar = new javax.swing.JMenuBar();
+        mnuJogo = new javax.swing.JMenu();
+        mnuPvp = new javax.swing.JMenuItem();
+        mnuPvm = new javax.swing.JMenuItem();
+        mnuMdoente = new javax.swing.JMenuItem();
+        mnuSob = new javax.swing.JMenu();
+        mnuSobre = new javax.swing.JMenuItem();
+        mnuSair = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -180,6 +224,51 @@ public class Principal extends javax.swing.JFrame {
         lblAtaul.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblAtaul.setText("X");
 
+        mnuJogo.setText("Novo Jogo");
+
+        mnuPvp.setText("P.v.P.");
+        mnuPvp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuPvpActionPerformed(evt);
+            }
+        });
+        mnuJogo.add(mnuPvp);
+
+        mnuPvm.setText("P.v.M.");
+        mnuPvm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuPvmActionPerformed(evt);
+            }
+        });
+        mnuJogo.add(mnuPvm);
+
+        mnuMdoente.setText("Modo Doente");
+        mnuJogo.add(mnuMdoente);
+
+        mnuBar.add(mnuJogo);
+
+        mnuSob.setText("Sobre");
+
+        mnuSobre.setText("Sobre");
+        mnuSobre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuSobreActionPerformed(evt);
+            }
+        });
+        mnuSob.add(mnuSobre);
+
+        mnuSair.setText("Sair");
+        mnuSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuSairActionPerformed(evt);
+            }
+        });
+        mnuSob.add(mnuSair);
+
+        mnuBar.add(mnuSob);
+
+        setJMenuBar(mnuBar);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -212,7 +301,7 @@ public class Principal extends javax.swing.JFrame {
                     .addComponent(lblPonto1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(lblJogador1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(lblAtaul, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 82, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -254,6 +343,27 @@ public class Principal extends javax.swing.JFrame {
     private void btnDoisDoisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDoisDoisActionPerformed
         viraBotao(btnDoisDois, 8);
     }//GEN-LAST:event_btnDoisDoisActionPerformed
+
+    private void mnuSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuSairActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_mnuSairActionPerformed
+
+    private void mnuSobreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuSobreActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this, "Este é um jogo da velha dinamico, que vai de tabuleiro 3x3 até o quanto o monitor e a RAM aguentar. \nFeito por Cristian Robert e Douglas Morona", "Sobre", JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_mnuSobreActionPerformed
+
+    private void mnuPvpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuPvpActionPerformed
+        // TODO add your handling code here:
+        geraJogoPvp();
+    }//GEN-LAST:event_mnuPvpActionPerformed
+
+    private void mnuPvmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuPvmActionPerformed
+        // TODO add your handling code here:
+        geraJogoPvm();
+
+    }//GEN-LAST:event_mnuPvmActionPerformed
 
     /**
      * @param args the command line arguments
@@ -313,26 +423,28 @@ public class Principal extends javax.swing.JFrame {
             }
             qmJoga = !qmJoga;
         }
-        if(verificaVelha()){
+        if (verificaVelha()) {
             JOptionPane.showMessageDialog(this, "Putz, deu velha, vamos jogar de novo", "VELHA", JOptionPane.WARNING_MESSAGE);
         }
     }
-    
-    public boolean verificaVelha(){
-        for(String v : arrayJogo)
-            if(v != "X" && v != "O")
+
+    public boolean verificaVelha() {
+        for (String v : arrayJogo) {
+            if (v != "X" && v != "O") {
                 return false;
+            }
+        }
         return true;
     }
 
     public boolean verificaVitoria(String valorJogador) {
         for (int i = 0; i < tamTab; i++) {
-            if (verificaHorizontal(valorJogador, tamTab*i)) {
+            if (verificaHorizontal(valorJogador, tamTab * i)) {
                 return true;
             }
         }
         for (int i = 0; i < tamTab; i++) {
-            if (verificaVertical(valorJogador, tamTab+i)) {
+            if (verificaVertical(valorJogador, tamTab + i)) {
                 return true;
             }
         }
@@ -346,7 +458,7 @@ public class Principal extends javax.swing.JFrame {
     }
 
     public boolean verificaHorizontal(String valorJogador, int valor) {
-        for (int i = valor; i < valor+tamTab; i++) {
+        for (int i = valor; i < valor + tamTab; i++) {
             if (arrayJogo[i] != valorJogador) {
                 return false;
             }
@@ -397,5 +509,13 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel lblJogador2;
     private javax.swing.JLabel lblPonto1;
     private javax.swing.JLabel lblPonto2;
+    private javax.swing.JMenuBar mnuBar;
+    private javax.swing.JMenu mnuJogo;
+    private javax.swing.JMenuItem mnuMdoente;
+    private javax.swing.JMenuItem mnuPvm;
+    private javax.swing.JMenuItem mnuPvp;
+    private javax.swing.JMenuItem mnuSair;
+    private javax.swing.JMenu mnuSob;
+    private javax.swing.JMenuItem mnuSobre;
     // End of variables declaration//GEN-END:variables
 }
